62ac4d81e749a09ef4af4551ca26f6c1d07986a6
Baullin1767 <starkov.alexd@gmail.com>
2023-03-02 14:48:56 +0300

    Version 0.5

--- san_el_bot/parcers.py
+++
@@ -1,8 +0,0 @@
-# from aiogram.types import Message
-# from aiogram.dispatcher import FSMContext
-# from santecknika import parse_santexnic
-# from electrika import parse_electric
-
-
-# async def pars_santeh(message: Message, state: FSMContext):
-    
